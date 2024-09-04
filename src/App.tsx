2d39407d27a83921edf7bfc019e8847230d9d7b0
Carlos Zuniga <carlos.zuniga.contractor@realtor.com>
2024-09-04 08:15:35 -0600

    feat: adding test delete

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,19 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
